b7628f086f2112c740a118bb0fd8eb90ff799db8
Timo Polvinen <timo.polvinen@gmail.com>
2018-04-19 12:27:46 +0300

    3.21 asti

--- tehtavat-3/puhelin-backend/index.js
+++ tehtavat-3/puhelin-backend/index.js
@@ -3,6 +3,7 @@ const app = express()
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
 const cors = require('cors')
+const Person = require('./models/person')
 
 app.use(cors())
 app.use(bodyParser.json())
@@ -12,55 +13,53 @@ morgan.token('content', function (req, res) {
 })
 app.use(morgan(':method :url :content :status :res[content-length] - :response-time ms'))
 
-let persons = [
-  {
-    "name": "Arto Hellas",
-    "number": "040-123456",
-    "id": 1
-  },
-  {
-    "name": "Martti Tienari",
-    "number": "040-123456",
-    "id": 2
-  },
-  {
-    "name": "Arto Järvinen",
-    "number": "040-123456",
-    "id": 3
-  },
-  {
-    "name": "Lea Kutvonen",
-    "number": "040-123456",
-    "id": 4
-  }
-]
-
-
-
 app.get('/info', (req, res) => {
-  const amount = persons.length
-  res.send(`Puhelinluettelossa on ${amount} henkilön tiedot \n${new Date()}`)
+  var amount = 0
+  Person
+    .find({})
+    .then(persons => {
+      amount = persons.length
+      res.send(`Puhelinluettelossa on ${amount} henkilön tiedot \n${new Date()}`)
+    })
 })
 
 app.get('/api/persons', (req, res) => {
-  res.json(persons)
+  Person
+    .find({})
+    .then(persons => {
+      res.json(persons.map(Person.format))
+    })
+    .catch(err => {
+      console.log(err)
+      res.status.send({ error: err })
+    })
 })
 
 app.get('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id)
-  const person = persons.find(person => person.id === id)
-  if (person) {
-    res.json(person)
-  } else {
-    res.status(404).json({ error: 'content missing' })
-  }
+  Person
+    .findById(req.params.id)
+    .then(person => {
+      if (person) {
+        res.json(Person.format(person))
+      } else {
+        res.status(404).end()
+      }
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(404).send({ error: 'malformatted id' })
+    })
 })
 
 app.delete('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id)
-  persons = persons.filter(person => person.id !== id)
-
-  res.status(204).end()
+  Person
+    .findByIdAndRemove(req.params.id)
+    .then(result => {
+      res.status(204).end()
+    })
+    .catch(err => {
+      res.status(400).send({ error: err })
+    })
 })
 
 app.post('/api/persons', (req, res) => {
@@ -68,23 +67,47 @@ app.post('/api/persons', (req, res) => {
 
   if (body.name === undefined
     || body.number === undefined) {
-    return res.status(400).json({ error: 'no content' })
+    return res.status(400).json({ error: 'content missing' })
   }
 
-  if (persons.filter(person =>
-    person.name === body.name).length > 0) {
-    return res.status(400).json({error: 'duplicate name'})
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+  })
+
+  Person
+    .find({ name: person.name })
+    .then(serverPerson => {
+      console.log(serverPerson)
+      if (serverPerson.length === 0) {
+        person
+          .save()
+          .then(savedPerson => {
+            res.json(Person.format(savedPerson))
+          })
+      } else {
+        res.status(400).json({error: 'duplicate'})
+      }
+    })
+})
+
+app.put('/api/persons/:id', (req, res) => {
+  const body = req.body
+
+  const person = {
+    name: body.name,
+    number: body.number
   }
 
-  const person =
-    {
-      name: body.name,
-      number: body.number,
-      id: Math.floor(Math.random() * 65535)
-    }
-
-  persons = persons.concat(person)
-  res.json(person)
+  Person
+    .findByIdAndUpdate(req.params.id, person, { new: true })
+    .then(updatedPerson => {
+      res.json(Person.format(updatedPerson))
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(400).send({ error: err })
+    })
 })
 
 const PORT = process.env.PORT || 3001
---
+++ tehtavat-3/puhelin-backend/mongo.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose')
+
+require('dotenv').config()
+console.log(process.env)
+// korvaa url oman tietokantasi urlilla. ethän laita salasanaa Gothubiin!
+const url = process.env.MONGODB_URI
+
+mongoose.connect(url)
+
+const Person = mongoose.model('person', {
+  name: String,
+  number: String,
+  id: Number,
+})
+
+const args = process.argv
+
+if (args.length === 2) {
+  Person
+    .find({})
+    .then(result => {
+      console.log('puhelinluettelo:')
+      result.forEach(person => {
+        console.log(person.name, person.number)
+      })
+      mongoose.connection.close()
+    })
+} else {
+  const person = new Person({
+    name: args[2],
+    number: args[3],
+    id: Math.floor(Math.random() * 65535)
+  })
+
+  person
+    .save()
+    .then(response => {
+      console.log('lisätään henkilö ', args[2], ' numero ', args[3], ' luetteloon')
+      mongoose.connection.close()
+    })
+}
